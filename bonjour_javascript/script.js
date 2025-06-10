adb8fa40c2acb2940782efdef762e212d277e75e
Merwann_tech <merwannheret@gmail.com>
2025-06-10 10:20:04 +0200

    Update

---
+++ bonjour_javascript/script.js
@@ -0,0 +1,21 @@
+let firstname = prompt()
+// let message = `Bonjour ${firstname} !`
+let today = new Date()
+console.log(today.getHours())
+
+function sayHello(firstname,hour){
+    if (hour < 18) {
+        message = `Bonjour ${firstname} !`
+    }
+    else{
+        message = `Bonsoir ${firstname} !`
+    }
+    console.log(message)
+    return message
+}
+
+sayHello(firstname,11)
+sayHello(firstname,18)
+sayHello(firstname,17)
+
+document.querySelector('h1').innerText = sayHello(firstname,today.getHours());
